846eb3bb6ddda6ca6bd906c09ccc54abc04fdb19
s-hosoai <shintaro.hosoai@gmail.com>
2022-11-02 18:36:05 +0900

    add live editing

--- lib/swest_cms_web/live/markdown_editor.ex
+++ lib/swest_cms_web/live/markdown_editor.ex
@@ -2,22 +2,24 @@ defmodule SwestCmsWeb.MarkdownEditor do
   use SwestCmsWeb, :live_view
 
   def mount(_params, _session, socket) do
-    rawhtml = Earmark.as_html!("# Hello World!\n !aae ee! [sample](code) \n - aaa \n - bbb")
-    markdown = ""
-    {:ok, assign(socket, rawhtml: rawhtml, markdown: markdown)}
+    # rawhtml = Earmark.as_html!("# Hello World!\n !aae ee! [sample](code) \n - aaa \n - bbb")
+    # markdown = ""
+    {:ok, assign(socket, rawhtml: "", markdown: "")}
   end
 
   def render(assigns) do
     ~H"""
     <h1>Markdown Previewer</h1>
-    <%!-- <form phx-change="change" >
-    <input type="text" name="text" value={@markdown} />
-    </form> --%>
+    <form phx-change="change">
+    <textarea name="text" value="{@markdown}"/>
+    <button text="save" value="save" >save </button>
+    </form>
     <%= raw @rawhtml %>
     """
   end
 
-  def handle_event("change", %{"markdown" => markdown}, socket) do
-    {:noreply, assign(socket, markdown: markdown)}
+  def handle_event("change", %{"_target" => ["text"], "text" => text}, socket) do
+    rawhtml = Earmark.as_html!(text)
+    {:noreply, assign(socket, rawhtml: rawhtml, markdown: text)}
   end
 end
